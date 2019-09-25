19e820d2243f2da2a9f1e503ffeee7256330ee56
Anders Lehmann <anders@ase.au.dk>
2019-09-25 08:57:13 +0200

    update code

--- RLE/rle.py
+++ RLE/rle.py
@@ -1,5 +1,13 @@
+import time
+class MyException(Exception):
+    pass
 
-def rle(mess):
+def encode(mess):
+
+    if type(mess) != str:
+        raise MyException()
+    if not mess:
+        return ''
     old = mess[0]
     count = 1
     res = []
@@ -12,3 +20,33 @@ def rle(mess):
             old = c
     res.append(f'{count}{old}')
     return ''.join(res)
+
+def decode(mess):
+    res = ''
+    count = 0
+    for c in mess:
+        if c.isdigit():
+            count = count*10 + int(c)
+        else:
+            res += c*count
+            count = 0
+    return res
+
+if __name__ == '__main__':
+    import sys
+    argv = sys.argv
+    if '-e' in argv:
+        filename = argv[argv.index('-e')+1]
+        func = encode # func er en funktion. Her er det encode
+    elif '-d' in argv:
+        filename = argv[argv.index('-d')+1]
+        func = decode # func er en funktion. Her er det decode
+    else:
+        filename = argv[-1]
+        func = lambda x: decode(encode(x)) # func er en funktion. Her decode(encode(x))
+    with open(filename,'r') as f:
+        print(func(f.read()))
+    # Denne pause er nødvendig for at fuzzeren kan skelne
+    # mellem crash og succes (en fejl i fuzzzeren)
+    time.sleep(1)
+    exit(0)
--- RLE/test_rle.py
+++ RLE/test_rle.py
@@ -1,11 +1,42 @@
-
-from rle import rle
+import pytest
+from rle import encode,decode,MyException
+from hypothesis import given
+from hypothesis.strategies import text
 
 def test_rle():
-    assert rle('kkk') == '3k'
+    assert encode('kkk') == '3k'
 
-def test_rle2():
-    assert rle('kkkkkkoooo') == '6k4o'
+def test_encode2():
+    assert encode('kkkkkkoooo') == '6k4o'
 
 def test_empty():
-    assert rle('') == ''
+    assert encode('') == ''
+
+def test_emoji():
+    assert encode('😇😇😇😇😇😇😇😇😍😍😍😍😍😍😍') =='8😇7😍'
+
+def test_None():
+    with pytest.raises(MyException) as info:
+        encode(None) == ''
+
+def test_space():
+    assert encode('kkk   ooo') == '3k3 3o'
+
+def test_digit():
+    assert encode('www111111qqq') == '3w613q'
+
+def test_escape():
+    assert encode('\\'*4) == '4\\'
+
+def test_int():
+    with pytest.raises(MyException) as exc_info:
+        encode(123)
+
+def test_decode():
+    assert decode('3b4,19 ') == 'bbb,,,,'+ ' '*19
+
+@given(text())
+def test_encode_decode(s):
+    #s = '2222aaaaaaaaafffffuuuu9999'
+    print(s)
+    assert decode(encode(s)) == s
