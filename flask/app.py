1f8fdb52034ba4045fbbb1adf15f2e8a80fc5998
Gubimand <malthe@bivs.dk>
2019-12-10 19:35:51 +0100

    Commented and changed app.py under /flask and made changes to volumes for db in compose file

--- flask/app.py
+++ flask/app.py
@@ -1,9 +1,17 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Flask Dockerized'
 
+# Creates the URL route
+@app.route('/')
+# Defines function for this route which will be executed
+def index1():
+    # returns a string to the webpage
+    return "Hello from flask"
+
+
+# main program
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    # Flask run command, initiates flask in debug mode and defines host.
+    app.run(debug=True, host='0.0.0.0')
